e88cbb6cf334603be28d9f660b4c00ec38f11e98
CyanSkyband <dragonsea07@gmail.com>
2025-09-16 18:25:38 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  // Create a canvas that fills the entire browser window
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+//This is the background  
+background(51,220,210);
+circle(380,270,100);
+}
